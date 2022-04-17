8a82b6aae81352221b0fef3baeeeaf1e95e4af1f
hasegawa-m-prog <hasegawa83838376@gmail.com>
2022-04-17 20:14:51 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,27 @@
 class ItemsController < ApplicationController
+
+  def index
+    @items = Item.order("created_at DESC") 
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+       redirect_to root_path
+    else
+       render :new
+     end
+   end
+  
+
+  private
+
+  def item_params
+    params.require(:item).permit(:product, :explanation, :category_id, :status_id, :postage_id, :prefecture_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root  'items#index'
   get 'items', to: 'items#index'
+
+  resources :items
 end
