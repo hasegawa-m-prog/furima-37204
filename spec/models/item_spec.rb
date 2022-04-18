ad08196a1473030343a05dbea4a8c2d205422e84
hasegawa-m-prog <hasegawa83838376@gmail.com>
2022-04-18 21:17:34 +0900

    商品出品機能の実装（テストコードの修正）

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,6 +11,16 @@ RSpec.describe Item, type: :model do
       end
             
     context '商品出品ができないとき' do
+      it 'userが紐付いていないと保存できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
+      it '画像が空では保存できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
       it '商品名が空では登録できない' do
         @item.product = ''
         @item.valid?
